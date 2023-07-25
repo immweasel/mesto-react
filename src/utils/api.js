20dbdab368b55a85fbd4507c4b16d713d3a28df1
Кира Максимова 211-323 <immweasel@yandex.ru>
2023-07-25 16:23:35 +0300

    fix: раскоммитить

--- src/utils/api.js
+++ src/utils/api.js
@@ -25,70 +25,70 @@ class Api {
     .then(this._checkResponse)
   }
 
-  // setUserInfo(data) {
-  //   return fetch(`${this._url}/users/me`, {
-  //     method: 'PATCH',
-  //     headers: this._headers,
-  //     body: JSON.stringify({
-  //       name: data.name,
-  //       about: data.description
-  //     })
-  //   })
-  //     .then(this._checkResponse)
-  // }
+  setUserInfo(data) {
+    return fetch(`${this._url}/users/me`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: data.name,
+        about: data.description
+      })
+    })
+      .then(this._checkResponse)
+  }
 
-  // setNewAvatar(data) {
-  //   return fetch(`${this._url}/users/me/avatar`, {
-  //     method: 'PATCH',
-  //     headers: this._headers,
-  //     body: JSON.stringify({
-  //       avatar: data.avatar
-  //     })
-  //   })
-  //     .then(this._checkResponse)
-  // }
+  setNewAvatar(data) {
+    return fetch(`${this._url}/users/me/avatar`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        avatar: data.avatar
+      })
+    })
+      .then(this._checkResponse)
+  }
 
-//   addCard(data) {
-//     return fetch(`${this._url}/cards`, {
-//       method: 'POST',
-//       headers: this._headers,
-//       body: JSON.stringify({
-//         name: data.title,
-//         link: data.link
-//       })
-//     })
-//       .then(this._checkResponse)
-//   }
+  addCard(data) {
+    return fetch(`${this._url}/cards`, {
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: data.title,
+        link: data.link
+      })
+    })
+      .then(this._checkResponse)
+  }
 
-//   addLike(cardId) {
-//     return fetch(`${this._url}/cards/${cardId}/likes`, {
-//       method: 'PUT',
-//       headers: {
-//         authorization: this._authorization
-//       }
-//     })
-//       .then(this._checkResponse)
-//   }
+  addLike(cardId) {
+    return fetch(`${this._url}/cards/${cardId}/likes`, {
+      method: 'PUT',
+      headers: {
+        authorization: this._authorization
+      }
+    })
+      .then(this._checkResponse)
+  }
 
-//   deleteLike(cardId) {
-//     return fetch(`${this._url}/cards/${cardId}/likes`, {
-//       method: 'DELETE',
-//       headers: {
-//         authorization: this._authorization
-//       }
-//     })
-//       .then(this._checkResponse)
-//   }
+  deleteLike(cardId) {
+    return fetch(`${this._url}/cards/${cardId}/likes`, {
+      method: 'DELETE',
+      headers: {
+        authorization: this._authorization
+      }
+    })
+      .then(this._checkResponse)
+  }
 
-//   deleteCard(cardId) {
-//     return fetch(`${this._url}/cards/${cardId}`, {
-//       method: 'DELETE',
-//       headers: {
-//         authorization: this._authorization
-//       }
-//     })
-//       .then(this._checkResponse)
-//   }
+  deleteCard(cardId) {
+    return fetch(`${this._url}/cards/${cardId}`, {
+      method: 'DELETE',
+      headers: {
+        authorization: this._authorization
+      }
+    })
+      .then(this._checkResponse)
+  }
 }
 
 const api = new Api({
